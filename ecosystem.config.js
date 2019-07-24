248c7bf7a03f75faf63ae830dd2ad4428bf35e6a
Stefano Pezzino <spezzino.13@gmail.com>
2019-07-24 20:51:01 +1200

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps : [{
+    name: "sorteador",
+    script: "./bin/www",
+    watch       : true,
+    env: {
+      NODE_ENV: "development",
+      DEBUG: "sorteador:*"
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
